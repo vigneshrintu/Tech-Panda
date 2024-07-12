c8b49c42eedbcc0daa156b4f2b83ed9b56ee92ce
Vigneshrintu <vigneshrintu@gmail.com>
2024-07-12 23:02:22 +0530

    changed vercel.json and next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['img.clerk.com']
+    images: {
+        domains: ['img.clerk.com']
+    },
+    experimental: {
+        runtime: 'nodejs',  // Ensure this is set to 'nodejs' instead of 'edge'
     }
 }
 
