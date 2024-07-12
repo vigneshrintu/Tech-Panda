26a7c6eac1561f0fbfbd2d8b74180ea33caa5e5f
Vigneshrintu <vigneshrintu@gmail.com>
2024-07-13 00:23:46 +0530

    just changed the middleware and commented the old part

--- client/middleware.ts
+++ client/middleware.ts
@@ -7,7 +7,10 @@ export default authMiddleware({
   publicRoutes:['/','/home']
 });
  
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
  
